d4ce94612b22a131e7f3af016861254e08a8aa81
Hannah-Jung <hj.hannahjung@gmail.com>
2025-10-29 20:25:24 -0700

    Update ProductAll.jsx

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -5,7 +5,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 const ProductAll = () => {
   const [productList, setProductList] = useState([])
   const getProducts = async() => {
-    let url = `https://my-json-server.typicode.com/Hannah-Jung/hnm-react-router-practice/products/${id}`
+    let url = `https://my-json-server.typicode.com/Hannah-Jung/hnm-react-router-practice/products`
     let response = await fetch(url)
     let data = await response.json()
     console.log(data)
